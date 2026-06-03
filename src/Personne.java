e37572691c4bffe19523b02ba96c971b5613632e
OUATTARA ELKANA <elkanaouattara@gmail.com>
2026-06-03 21:40:15 +0000

    Creation du projet Personne Creation de la class Personne Compteurs static et non static et incrementation des deux compteurs Creation de la fonction Main. Creation de quatre instance de personne.

--- src/Personne.java
+++ src/Personne.java
@@ -11,5 +11,15 @@ public class Personne {
 
     }
 
+    public static void main(String[] args) {
+        Personne personne1 = new Personne();
+
+        Personne personne2 = new Personne();
+
+        Personne personne3 = new Personne();
+
+        Personne personne4 = new Personne();
+    }
+
 
 }
