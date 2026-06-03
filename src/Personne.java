9eb565eeb0e620ff4b318651ab83ded1e7e61309
OUATTARA ELKANA <elkanaouattara@gmail.com>
2026-06-03 21:37:30 +0000

    Creation du projet Personne Creation de la class Personne Compteurs static et non static et incrementation des deux compteurs

--- src/Personne.java
+++ src/Personne.java
@@ -1,2 +1,15 @@
 public class Personne {
+    // Déclaration des compteurs
+    public static int nbInstances;
+    public int nbLocal;
+
+    public Personne(){
+
+        nbInstances++;
+
+        nbLocal++;
+
+    }
+
+
 }
